b445cc2495e500472010bda97016b54bce61f716
Marcio Carvalho <marciocarvalho@marybook.lan>
2023-02-02 23:30:33 -0800

    Rename folders

---
+++ src/test/kotlin/com/minone/bogus/BogusApplicationTests.kt
@@ -0,0 +1,11 @@
+package com.minone.bogus
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest(classes = [BogusApplication::class])
+class BogusApplicationTests {
+    @Test
+    fun contextLoads() {
+    }
+}
