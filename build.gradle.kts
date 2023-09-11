721e88afb656b5b14fc3eb761c63d60d37d4eb14
Marcio Carvalho <marweck@gmail.com>
2023-09-10 22:57:47 -0700

    Upgrade spring boot to 3.2M2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,46 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "3.1.0"
-    id("io.spring.dependency-management") version "1.1.0"
-    id("org.graalvm.buildtools.native") version "0.9.20"
-    kotlin("jvm") version "1.8.21"
-    kotlin("plugin.spring") version "1.8.21"
+    id("org.springframework.boot") version "3.2.0-M2"
+    id("io.spring.dependency-management") version "1.1.3"
+    id("org.graalvm.buildtools.native") version "0.9.26"
+    kotlin("jvm") version "1.9.10"
+    kotlin("plugin.spring") version "1.9.10"
 }
 
 group = "com.minone"
 version = "0.0.1"
 description = "bogus"
-java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
     mavenCentral()
+    maven { url = uri("https://repo.spring.io/milestone") }
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_20
+}
+
+graalvmNative {
+    binaries {
+        named("main") {
+            buildArgs.add("--enable-preview")
+            buildArgs.add("-J-Xmx7g")
+        }
+    }
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        // Null-safety for Spring API in Kotlin
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        // Highest so far Java version supported by Kotlin
+        jvmTarget = "20"
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
 }
 
 dependencies {
@@ -23,18 +49,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.gatling.highcharts:gatling-charts-highcharts:3.9.0")
-}
-
-tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "17"
-    }
-}
-
-tasks.withType<Test> {
-    useJUnitPlatform()
 }
